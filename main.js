375cf924aedf0837d14f920de4b883d5e86deba2
Acerx-AMJ <ajm.acerx@gmail.com>
2026-01-15 14:07:21 +0200

    Added player movement and collision.

--- main.js
+++ main.js
@@ -9,25 +9,63 @@ https://sprig.hackclub.com/gallery/getting_started
 */
 
 const player = "p"
+const wall = "w"
+const background = "b"
 
 setLegend(
   [ player, bitmap`
-................
-................
-.......999......
-......99.9......
-......9..99.....
-......0...0.0...
-....0003.30.0...
-....0.0...000...
-....0.05550.....
+.....0000000....
+.....0000000....
+....000000000...
+....C9999999C...
+....C9929299C...
+....C0999990C...
+......00000.....
+......50005.....
+.....5555555....
+....5.55555.5...
+....5.55555.5...
+....9.55555.9...
+......00000.....
+......00000.....
 ......0...0.....
-.....0....0.....
-.....0...0......
-......000.......
-......0.0.......
-.....00.00......
-................` ]
+......0...0.....` ], [
+  wall, bitmap`
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000`
+  ], [
+    background, bitmap`
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL`
+  ]
 )
 
 setSolids([])
@@ -35,8 +73,9 @@ setSolids([])
 let level = 0
 const levels = [
   map`
-p.
-..`
+pw.
+.w.
+...`
 ]
 
 setMap(levels[level])
@@ -51,6 +90,15 @@ onInput("s", () => {
 onInput("w", () => {
   getFirst(player).y -= 1
 })
+onInput("d", () => {
+  getFirst(player).x += 1
+})
+onInput("a", () => {
+  getFirst(player).x -= 1
+})
+
+setBackground(background)
+setSolids([player, wall])
 
 afterInput(() => {
   

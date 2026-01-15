6c232679aea1d6d52c0ebc3d18c4cd78b140ff4f
Acerx-AMJ <ajm.acerx@gmail.com>
2026-01-15 17:49:03 +0200

    Changed some sprites. Added a reset button.

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
-
 @title: Escape NYC
 @author: 
 @tags: []
@@ -9,32 +6,39 @@ https://sprig.hackclub.com/gallery/getting_started
 */
 
 /*
+  KEYBINDS
+  w, a, s, d - move
+  i - reset level
+*/
+
+/*
   INITIALIZATION
 */
 
-const player = "p"
-const wall = "w"
-const background = "b"
-const enemy = "l"
+const player     = "p",
+      wall       = "w",
+      background = "b",
+      enemy      = "l",
+      coin       = "c";
 
 setLegend(
   [ player,     bitmap`
-.....0000000....
-.....0000000....
-....000000000...
-....C9999999C...
-....C9929299C...
-....C0999990C...
-......00000.....
-......50005.....
-.....5555555....
-....5.55555.5...
-....5.55555.5...
-....9.55555.9...
-......00000.....
-......00000.....
-......0...0.....
-......0...0.....` ],
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555` ],
   [ wall,       bitmap`
 0000000000000000
 0000000000000000
@@ -70,38 +74,53 @@ LLLLLLLLLLLLLLLL
 LLLLLLLLLLLLLLLL
 LLLLLLLLLLLLLLLL` ],
   [ enemy,      bitmap`
-................
-................
-....0000000.....
-....9900000.....
-....9999000.....
-....9929290.....
-....9999999.....
-....9900099.....
-....9990999.....
-......0L0.......
-.....00000......
-....0.000.3.....
-....0.000.0.....
-....9.000.9.....
-......0.0.......
-......0.0.......` ],
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333` ],
+  [ coin,       bitmap`
+......0000......
+....00222200....
+...0666622220...
+..066666666220..
+..066600006620..
+.0F6606FFF06220.
+.0F660266F06220.
+.0F660266F06620.
+.0F660266F06620.
+.0F660266F06620.
+.0FF60222606620.
+..0F6600006660..
+..0FF666666660..
+...0FFF666660...
+....00FFFF00....
+......0000......` ],
 )
 
 let level = 0
-let switchToGame = false
-
 const levels = [
   map`.`,
   map`
 .............
 .............
-......l......
+..c...l......
 .............
 .............
 .............
-.............
-....pw.......
+.........cc..
+....pw...cc..
 .....w.......
 .............
 .............
@@ -113,8 +132,10 @@ const levels = [
 
 setMap(levels[level])
 setBackground(background)
-setSolids([player, wall])
+setSolids([player, wall, coin])
 
+// Start the game with the main menu which simply
+// contains hard-coded text.
 function setupMainMenu() {
   addText("ESCAPE NYC", {x: 5, y: 5, color: color`2`})
   addText("PRESS ANY KEY", {x: 4, y: 8, color: color`2`})
@@ -146,8 +167,27 @@ onInput("a", () => {
   getFirst(player).x -= 1
 })
 
+// Reset the level unless in the main menu
+onInput("i", () => {
+  if (level == 0) return
+  setMap(levels[level])
+})
+
+onInput("j", () => {
+  if (level == 0) return
+})
+
+onInput("k", () => {
+  if (level == 0) return
+})
+
+onInput("l", () => {
+  if (level == 0) return
+})
+
 afterInput(() => {
   // Switch from main menu to the first level
+  // (a key has been pressed)
   if (level == 0) {
     clearText()
     level += 1
@@ -161,7 +201,5 @@ afterInput(() => {
 */
 
 setInterval(() => {
-  if (level === 0) {
-    
-  }
+
 }, 100)

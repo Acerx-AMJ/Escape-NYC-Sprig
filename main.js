6d4950506e478df25fc348dfb43933eb5738ddad
Acerx-AMJ <ajm.acerx@gmail.com>
2026-01-17 22:22:30 +0200

    Added dying.

--- main.js
+++ main.js
@@ -130,43 +130,62 @@ const levels = [
 .............`
 ]
 
-let collectedCoins = 0,
-    maximumCoins   = 0;
+let collectedCoins  = 0,
+    maximumCoins    = 0,
+    canRetryOnDeath = false,
+    playerHasDied   = false;
 
 setMap(levels[level])
 setBackground(background)
+
+// Set collisions
 setSolids([player, wall])
 
 // Start the game with the main menu which simply
 // contains hard-coded text.
 function setupMainMenu() {
+  clearText()
   addText("ESCAPE NYC", {x: 5, y: 5, color: color`2`})
   addText("PRESS ANY KEY", {x: 4, y: 8, color: color`2`})
   addText("TO START", {x: 6, y: 10, color: color`2`})
 }
 setupMainMenu()
 
+// Draw the same hard-coded text when the player
+// looses.
+function setupLoseScreen() {
+  clearText()
+  addText("YOU DIED!", {x: 6, y: 5, color: color`2`})
+  addText("PRESS ANY KEY", {x: 4, y: 8, color: color`2`})
+  addText("TO RETRY", {x: 6, y: 10, color: color`2`})
+}
+
 // Setup the necessary variables for a new level.
 // Call whenever levels get switched
 function initGameLevel() {
-  collectedCoins = 0
-  
-  // Count the coint count in the map. I don't
-  // know JS. What the fuck is this syntax??
-  maximumCoins = 0
-  for (a in levels[level]) {
-    if (levels[level][a] == coin) maximumCoins += 1
-  }
-  
   setMap(levels[level])
+  
+  collectedCoins = 0
+  maximumCoins = getAll(coin).length;
   drawGameText()
 }
 
 // Draw game UI like the coin counter
 function drawGameText() {
-  // Clear existing text first
   clearText();
   addText("COINS: " + collectedCoins + "/" + maximumCoins, {x: 3, y: 0, color: color`2`})
+}
+
+// Kill the player and show the death screen.
+function killPlayer() {
+  playerHasDied = true
+  canRetryOnDeath = false
+  setTimeout(() => {canRetryOnDeath = true}, 500)
+  
+  getFirst(player).remove()
+  level = 0
+  setMap(levels[level])
+  setupLoseScreen()
 }
 
 /*
@@ -215,6 +234,11 @@ afterInput(() => {
   // Switch from main menu to the first level
   // (a key has been pressed)
   if (level == 0) {
+    // Small timeout in case they accidentally press
+    // a button.
+    if (playerHasDied && !canRetryOnDeath) return
+    
+    playerHasDied = false
     level += 1
     initGameLevel()
     return
@@ -222,14 +246,22 @@ afterInput(() => {
 
   // Handle colleting coins. increment the coin
   // counter and delete the tile
-  const { y: y, x : x } = getFirst(player);
+  const { y: y, x: x } = getFirst(player)
+  const coinTile = getTile(x, y).find((tile) => tile.type === coin)
   
-  // What the fuck even is JS??
-  if (getTile(x, y).some(({type}) => type === coin)) {
-    clearTile(x, y)
-    addSprite(x, y, player) // Place player back
+  if (coinTile) {
+    coinTile.remove()
     collectedCoins += 1
     drawGameText()
+  }
+
+  // Handle dying. Switch to the 'main menu' and
+  // start the game from the beginning. The player
+  // stepped on an enemy. What a dumbass.
+  const enemyTile = getTile(x, y).find((tile) => tile.type === enemy)
+
+  if (enemyTile) {
+    killPlayer()
   }
 })
 
@@ -238,5 +270,29 @@ afterInput(() => {
 */
 
 setInterval(() => {
+  if (level == 0) return
+  const { y: y, x: x } = getFirst(player)
 
-}, 100)
+  getAll(enemy).map(function (e) {
+    // Handle moving enemies to the player
+    let ny = e.y,
+        nx = e.x;
+    
+    if (e.y < y) ny += 1
+    if (e.y > y) ny -= 1
+    if (e.x < x) nx += 1
+    if (e.x > x) nx -= 1
+
+    // Manual collision because the engine is really
+    // limiting. I guess that's a good thing?
+    if (!getTile(nx, ny).some((tile) => tile.type === wall || tile.type === enemy)) {
+      e.x = nx
+      e.y = ny
+    }
+    
+    // Enemy stepped on the player
+    if (y == ny && x == nx) {
+      killPlayer()
+    }
+  });
+}, 1000)
